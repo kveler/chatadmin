0e728c2e8c186eb0d28ce8304c372557a5fdc53c
Max Veenstra <m.veenstra@compra.nl>
2016-03-24 15:39:12 +0100

    Hier gaat iets niet goed
    RIP

--- system/interfaces/isystemcomponent.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface ISystemComponent
-{
-
-    static function getInstance ();
-    
-}
--- system/interfaces/isystemcomponentdatacompatible.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface ISystemComponentDataCompatible
-{
-
-    static function getInstance ($data);
-
-}
